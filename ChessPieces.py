e66eb3a31fa537e483be9fe8c28d25ba958846a7
carlosgarcia10001 <55305519+carlosgarcia10001@users.noreply.github.com>
2019-09-14 00:39:40 -0700

    Add files via upload

---
+++ ChessPieces.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class ChessPiece:
+    NONE = 0
+    PAWN = 1
+    KNIGHT = 2
+    KING = 3
+    QUEEN = 4
+    BISHOP =5
+    ROOK = 6
---
+++ Color.py
@@ -0,0 +1,6 @@
+from enum import Enum
+class PieceColor(Enum):
+    NONE = 0
+    WHITE = 1
+    BLACK = 2
+
