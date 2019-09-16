478a1192d730659353be8bee946b7309bbae1eba
carlosgarcia10001 <55305519+carlosgarcia10001@users.noreply.github.com>
2019-09-15 18:02:11 -0700

    Update SquareClickDetection.py

--- SquareClickDetection.py
+++ SquareClickDetection.py
@@ -2,7 +2,7 @@ SQUARELENGTH = 80
 def coordinatestosquareindex(x,y): #Used to convert x,y coordinates to square IDs. First square is ID 0, last square is ID 63
     return y//SQUARELENGTH*8+x//SQUARELENGTH
 def squareindextocoordinates(square): #Used to convert square IDs to the first x,y coordinate of a square.
-    return (80*(square%8),80*(square//8))
+    return (SQUARELENGTH*(square%8),SQUARELENGTH*(square//8))
 def squaretocoordinatespiecelocation(square): #Used to place the actual Chess pieces.
-    return (10+80*(square%8),10+80*(square//8))
+    return (10+SQUARELENGTH*(square%8),10+SQUARELENGTH*(square//8))
 
